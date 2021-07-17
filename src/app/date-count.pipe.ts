0473cadbe3718367491e1877b0d155c63f49efab
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-07-17 18:44:17 +0300

    Create custom  pipes

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
