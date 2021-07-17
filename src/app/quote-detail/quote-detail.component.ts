bf6c2e2095a3d04e307d447af28221f4d2741d01
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-07-17 16:29:41 +0300

    generate new quote-detail component

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input} from '@angular/core';
+import {Quote}
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
